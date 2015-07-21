40dbde42b7aec4d741f7a3dc59fa5977204cba72
Niklas Olmes <nolmes@uni-paderborn.de>
2015-07-21 11:42:06 +0200

    fix UI issue where contracts and/or shop could be openend simultaneously: disable controls while in shop or contracts

--- manager/public_html/display.js
+++ manager/public_html/display.js
@@ -1,13 +1,17 @@
 function openContracts (){
     document.getElementById("contracts").style.setProperty("display", "inherit");
+    document.getElementById("control").style.setProperty("display", "none");
 }
 function openShop (){
     document.getElementById("shop").style.setProperty("display", "inherit");
+    document.getElementById("control").style.setProperty("display", "none");
 }
 function closeContracts (){
     document.getElementById("contracts").style.setProperty("display", "none");
+    document.getElementById("control").style.setProperty("display", "inherit");
 }
 function closeShop (){
     document.getElementById("shop").style.setProperty("display", "none");
+    document.getElementById("control").style.setProperty("display", "inherit");
 }
 

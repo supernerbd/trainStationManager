1a302c85e6d5d251add7782f633aa9ee2b3e7cff
Niklas Olmes <nolmes@uni-paderborn.de>
2015-07-24 14:32:38 +0200

    forgot the class file

---
+++ manager/public_html/gameState.js
@@ -0,0 +1,16 @@
+function GameState() {
+	this.acceptedContracts = new Array();
+	this.daysPlayed = 0;
+	this.money = 0;
+	this.offeredContracts = new Array();
+	this.numPlatforms = 0;
+	this.stopped = true;
+	this.nextLine = 1;
+	this.speed = 1;
+	this.table = new Array();
+	this.events = new Array();
+	this.dayTraffic = new Array();
+	this.time = 0;
+
+	return this;
+}

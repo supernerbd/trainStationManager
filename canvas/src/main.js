1cb2d0500c6e72e5fd5fe648b99245d954918d36
B <supernerbd@yahoo.de>
2015-10-31 18:22:43 +0100

    first try

--- canvas/src/main.js
+++ canvas/src/main.js
@@ -1,7 +1,30 @@
 //main.js
 'use strict'
 
-game.main = function(){
+game.main = (function(){
+	//vars
+	var canvas;
+	var ctx;
+	//constants
+	var CANVAS = Object.freeze({
+		HEIGHT: window.innerHeight,
+		WIDTH: window.innerWidth,
+	}); 
+	//objs
+	
+	//methods
+	
+	function init(){
+		canvas = document.querySelector('canvas');
+		canvas.width = CANVAS.WIDTH;
+		canvas.height = CANVAS.HEIGHT;
+		ctx = canvas.getContext('2d');
+		ctx.fillRect(0,0,CANVAS.WIDTH,CANVAS.HEIGHT);
+	};
 
-
-};
+	return {
+		init: init,
+		CANVAS: CANVAS,
+		ctx: ctx,
+	}
+}());
--- canvas/src/ui.js
+++ canvas/src/ui.js
@@ -1,7 +1,32 @@
 //ui.js
 'use strict'
 
-game.ui = function(){
-
-
-};
+game.ui = (function(){
+	//vars
+	var CANVAS;
+	var ctx;
+	var buttons = [];
+	//objs
+	function Button(name,x,y,width,height,action){
+		this.name: name,
+		this.x=x;
+		this.y=y;
+		this.width=width;
+		this.height=height;
+		this.action=action;
+		return this;
+	}
+	//methods
+	
+	function init(){
+		CANVAS=game.main.CANVAS;
+		ctx=game.main.ctx;
+	};
+	
+	function createButtons(){
+		//create all Buttons in here and store them in array buttons. On mouse click search array
+	};
+	return {
+		init: init,
+	}
+}());
